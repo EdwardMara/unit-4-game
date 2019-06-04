5c65205e5a78a3fb5888d44687891abb27145159
Edward Mara <e.james.mara@gmail.com>
2019-06-04 16:27:11 -0700

    html layout and initial variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+//points that each gem are worth
+//randomized every round;
+var gem1Points;
+var gem2Points;
+var gem3Points;
+var gem4Points;
+
+//random point goal
+var pointGoal;
+
+//current user points
+var currentPoints = 0;
+
+//wins and losses
+var wins = 0;
+var losses = 0;
+
+
+
+
+
